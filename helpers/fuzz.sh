a9078d7345206e12f3f1a00decc86ec0b569ab4d
ShuoHe97 <she77@wisc.edu>
2022-02-25 13:39:37 -0600

    readme

--- helpers/fuzz.sh
+++ helpers/fuzz.sh
@@ -1,4 +1,32 @@
 echo "usage bash fuzz.sh \$engine \$method \$in \$out \$cacheDir \$testclass "
+
+# Example:
+# bash helpers/fuzz.sh tf testFaceDetection ./data ./results $(pwd)/results/instrumentation edu.ucla.cs.FaceTest
+
+
+# Explain:
+# $engine: "tf" for our customized guidance.
+
+# $method: is the test method we want to run. For example: "testFaceDetection" as defined in FaceTest.java
+
+# $in: is the directory that consists of test input images.
+
+# $out: is the directory that we want the results to be stored
+
+# $cashDir: is the directory to store temp files
+
+# $testclass: is the class of testcase. For example: "edu.ucla.cs.FaceTest" as defined in FaceTest.java
+
+
+
+
+
+
+
+
+
+
+
 mkdir -p $4
 #mvn jqf:repro -Dclass=edu.ucla.cs.AppTest -DincludeOnly=edu.ucla.cs,net.tzolov,org/nd4j/tensorflow/conversion/graphrunner/ -Dinput=$1 -Dmethod=testProgramWithModel -Dengine=$2  -Djanala.verbose=true -Dout=$3
 
--- helpers/repro.sh
+++ helpers/repro.sh
@@ -1,4 +1,26 @@
 echo "usage bash repro.sh \$input \$engine \$out \$method"
+
+# Example
+# bash helpers/repro.sh ./results/corpus tf ./results/repro testFaceDetection edu.ucla.cs.FaceTest
+
+# $input: is the /corpus directory inside the previous test results directory
+
+# $engine: "tf" for our customized guidance.
+
+# $out: is the output directory for reproduced results to be stored
+
+# $method: is the test method we want to re-run.
+
+
+
+
+
+
+
+
+
+
+
 #rm -r $3/instrumentation
 
 mvn -e -X jqf:repro -Dclass=$5 -DincludeOnly=edu.ucla.cs.FaceTest,edu.ucla.cs.CropTest,edu.ucla.cs.PoseTest,net.tzolov -Dinput=$1 -Dmethod=$4 -Dengine=$2  -Djanala.verbose=true -Dout=$3 -Djanala.instrumentationCacheDir="$3/instrumentation" -Djqf.tracing.TRACE_GENERATORS=true
